a423d385ef89cfa0869c314bd07f9af213c62b04
okonayakodji <120666729+okonayakodji@users.noreply.github.com>
2023-09-17 11:01:49 +0300

    Initial commit

---
+++ src/main/java/smallsecret/SmallSecretMod.java
@@ -0,0 +1,14 @@
+package smallsecret;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SmallSecretMod implements ModInitializer {
+    public static final String MOD_ID = "smallsecret";
+	public static final Logger LOGGER = LoggerFactory.getLogger("smallsecret");
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/smallsecret/SmallSecretModDataGenerator.java
@@ -0,0 +1,11 @@
+package smallsecret;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SmallSecretModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
